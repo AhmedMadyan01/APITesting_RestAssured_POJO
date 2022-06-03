f984ecc86cc20fc8c3f8c3d93318b66ea7c408c3
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-03 03:47:46 +0200

    Update Support.java

--- src/main/java/api/POJOResponsePayloads/Users/Support.java
+++ src/main/java/api/POJOResponsePayloads/Users/Support.java
@@ -3,4 +3,20 @@ package api.POJOResponsePayloads.Users;
 public class Support {
     public String url;
     public String text;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
