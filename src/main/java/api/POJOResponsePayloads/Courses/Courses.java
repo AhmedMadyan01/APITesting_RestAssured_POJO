f5ff45fce7ffafef43e1fde5877f99b6918dc5e2
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-03 03:23:45 +0200

    Update Courses.java

--- src/main/java/api/POJOResponsePayloads/Courses/Courses.java
+++ src/main/java/api/POJOResponsePayloads/Courses/Courses.java
@@ -1,6 +1,18 @@
 package api.POJOResponsePayloads.Courses;
 
 public class Courses {
+    public WebAutomation getWebAutomation() {
+        return webAutomation;
+    }
+
+    public API getApi() {
+        return api;
+    }
+
+    public Mobile getMobile() {
+        return mobile;
+    }
+
     private WebAutomation webAutomation;
     private API api;
     private Mobile mobile;
