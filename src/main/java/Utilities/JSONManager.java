7fa281a13a8ae633964956e947c55fa90491145b
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-03 18:36:56 +0200

    Update JSONManager.java

--- src/main/java/Utilities/JSONManager.java
+++ src/main/java/Utilities/JSONManager.java
@@ -8,12 +8,15 @@ import org.json.simple.parser.ParseException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
 public class JSONManager {
 
     private static FileReader reader;
-    private static JSONParser parser;
-    JsonPath jsonPath;
+    private static final JSONParser parser = new JSONParser();
+
+    private static JsonPath jsonPath;
     private static JSONObject object = null;
 
     public static JSONObject readJSONFile(String filePath) throws IOException, ParseException {
@@ -28,22 +31,43 @@ public class JSONManager {
         return object;
     }
 
-    public static void getJSONData(JSONObject object, String keyPath, Types type) {
+    public static void getJSONData(JSONObject object, String keyPath, ReturnDataTypes type) {
 
     }
 
-    public static void getJSONData(String filePath, String keyPath, Types type) throws IOException, ParseException {
+    public static Object getJSONData(String filePath, String keyPath, ReturnDataTypes type) throws IOException, ParseException {
         object = readJSONFile(filePath);
-
+        Object data;
+        switch (type) {
+            case STRING -> data = getStringFromJSONObject(object, keyPath);
+            case HASHMAP -> data = getHashMapFromJSONObject(object, keyPath);
+            case LIST -> data = getListFromJSONObject(object, keyPath);
+            default -> throw new IllegalStateException("Unexpected value: " + type);
+        }
+        if (data != null)
+            return data;
+        else {
+            System.out.println("No data found");
+        }
+        return null;
     }
 
-    private String getStringFromJSONObject(JSONObject object, String keyPath) {
+    private static String getStringFromJSONObject(JSONObject object, String keyPath) {
         jsonPath = new JsonPath(String.valueOf(object));
-        jsonPath.getJsonObject(keyPath);
-        return jsonPath.toString();
+        return jsonPath.getJsonObject(keyPath).toString();
     }
 
-    private enum Types {
-        STRING, HASHMAP, LIST, LIST_HASHMAP
+    private static List<?> getListFromJSONObject(JSONObject object, String keyPath) {
+        jsonPath = new JsonPath(String.valueOf(object));
+        return jsonPath.getList(keyPath);
+    }
+
+    private static HashMap<?, ?> getHashMapFromJSONObject(JSONObject object, String keyPath) {
+        jsonPath = new JsonPath(String.valueOf(object));
+        return jsonPath.getJsonObject(keyPath);
+    }
+
+    public enum ReturnDataTypes {
+        STRING, HASHMAP, LIST
     }
 }
