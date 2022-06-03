4cc77dcf0589eba4f22b3deae7e50a18f531c6e7
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-03 03:45:43 +0200

    Update GetCourses.java

--- src/main/java/api/POJOResponsePayloads/Courses/GetCourses.java
+++ src/main/java/api/POJOResponsePayloads/Courses/GetCourses.java
@@ -14,23 +14,47 @@ public class GetCourses {
         return url;
     }
 
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
     public String getServices() {
         return services;
+    }
+
+    public void setServices(String services) {
+        this.services = services;
     }
 
     public String getExpertises() {
         return expertises;
     }
 
+    public void setExpertises(String expertises) {
+        this.expertises = expertises;
+    }
+
     public Courses getCourses() {
         return courses;
+    }
+
+    public void setCourses(Courses courses) {
+        this.courses = courses;
     }
 
     public String getInstructor() {
         return instructor;
     }
 
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
     public String getLinkedIn() {
         return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
     }
 }
