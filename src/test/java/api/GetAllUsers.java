cef71f06b88f53ca48c1ade193cbb361db754b7e
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-03 05:15:45 +0200

    Update GetAllUsers.java

--- src/test/java/api/GetAllUsers.java
+++ src/test/java/api/GetAllUsers.java
@@ -5,14 +5,26 @@ import io.restassured.RestAssured;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class GetAllUsers {
     @Test
-    public void getNumberOfCourses() {
+    public void getNoOfUsers() {
+        GetUsers getUsers =
+                RestAssured.given().when().get("https://reqres.in/api/users?page=2").as(GetUsers.class);
+        Assert.assertEquals(getUsers.getData().size(), 6);
+        System.out.println("No. of users:" + getUsers.getData().size());
+    }
+
+    @Test
+    public void getUsersFirstName() {
         GetUsers getUsers =
                 RestAssured.given().when().get("https://reqres.in/api/users?page=2").as(GetUsers.class);
         Assert.assertEquals(getUsers.getData().size(), 6);
         Assert.assertEquals(getUsers.getData().get(2).getFirst_name(), "Tobias");
-        Assert.assertTrue(getUsers.getData().contains("id"));
-        System.out.println(getUsers.getData().size());
+        for (int i = 0; i <= getUsers.getData().size() - 1; i++) {
+            System.out.println("User " + i + " First Name: " + getUsers.getData().get(i).getFirst_name());
+        }
     }
 }
