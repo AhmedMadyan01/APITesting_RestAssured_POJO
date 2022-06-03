3b48aa03c5cd8fb8aeb0a00241f2a846171e137d
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-03 03:00:32 +0200

    UPDATE

---
+++ src/main/java/api/POJOResponsePayloads/Courses/Courses.java
@@ -0,0 +1,7 @@
+package api.POJOResponsePayloads.Courses;
+
+public class Courses {
+    private WebAutomation webAutomation;
+    private API api;
+    private Mobile mobile;
+}
---
+++ src/main/java/api/POJOResponsePayloads/Courses/Mobile.java
@@ -0,0 +1,14 @@
+package api.POJOResponsePayloads.Courses;
+
+public class Mobile {
+    private String courseTitle;
+    private String price;
+
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+}
