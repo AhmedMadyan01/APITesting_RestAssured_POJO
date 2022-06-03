82c1198f96fcac9de38a9ecc686f77b80502540d
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-03 22:10:01 +0200

    Update JSONManager.java

--- src/main/java/utilities/JSONManager.java
+++ src/main/java/utilities/JSONManager.java
@@ -16,7 +16,12 @@ public class JSONManager {
     private static JsonPath jsonPath;
     private static JSONObject object = null;
 
-    public static JSONObject readJSONFile(String filePath) throws IOException, ParseException {
+    /**
+     * Read JSON File with File Reader
+     *
+     * @return FileReader type, JSON File stored in @param reader value
+     */
+    public static FileReader readFileJSON(String filePath) throws IOException, ParseException {
         if (filePath != null)
             try {
                 reader = new FileReader(filePath);
@@ -24,20 +29,89 @@ public class JSONManager {
                 throw new RuntimeException(e);
             }
         else System.out.println("Please send the JSON file path");
+        return reader;
+    }
+
+    /**
+     * Read JSON File with File Reader
+     *
+     * @return JSONObject data type, the desired JSON Object sent by @param filePath
+     */
+    public static JSONObject parseJSON(String filePath) throws IOException, ParseException {
         JSONParser parser = new JSONParser();
-        object = (JSONObject) parser.parse(reader);
+        object = (JSONObject) parser.parse(readFileJSON(filePath));
         return object;
     }
 
+    /**
+     * Extract the desired JSON Data based on the key path
+     *
+     * @param object
+     * @param keyPath
+     * @param type
+     * @return Object data type, the desired JSON Object sent by @param filePath
+     */
     public static Object getJSONData(JSONObject object, String keyPath, Types type) {
         return getObject(keyPath, type, object);
     }
 
+    /**
+     * Extract the desired JSON Data based on the key path
+     *
+     * @param filePath
+     * @param keyPath
+     * @param type
+     * @return Object data type, the desired JSON Object sent by @param filePath
+     */
     public static Object getJSONData(String filePath, String keyPath, Types type) throws IOException, ParseException {
-        object = readJSONFile(filePath);
+        object = parseJSON(filePath);
         return getObject(keyPath, type, object);
     }
 
+    /**
+     * Extract the desired JSON Data to String Value
+     *
+     * @param object
+     * @param keyPath
+     * @return String data type
+     */
+    private static String getDataAsString(JSONObject object, String keyPath) {
+        jsonPath = new JsonPath(String.valueOf(object));
+        return jsonPath.getJsonObject(keyPath).toString();
+    }
+
+    /**
+     * Extract the desired JSON Data to Map Value
+     *
+     * @param object
+     * @param keyPath
+     * @return Map  data type
+     */
+    private static Map<?, ?> getDataAsMap(JSONObject object, String keyPath) {
+        jsonPath = new JsonPath(String.valueOf(object));
+        return jsonPath.getMap(keyPath);
+    }
+
+    /**
+     * Extract the desired JSON Data to List Value
+     *
+     * @param object
+     * @param keyPath
+     * @return List data type
+     */
+    private static List<?> getDataAsList(JSONObject object, String keyPath) {
+        jsonPath = new JsonPath(String.valueOf(object));
+        return jsonPath.getList(keyPath);
+    }
+
+    /**
+     * Retrieve the desired JSON Object from the desired keyPath
+     *
+     * @param keyPath
+     * @param type
+     * @param object
+     * @return Object data type
+     */
     private static Object getObject(String keyPath, Types type, JSONObject object) {
         Object data;
         switch (type) {
@@ -54,22 +128,8 @@ public class JSONManager {
         return null;
     }
 
-    private static String getDataAsString(JSONObject object, String keyPath) {
-        jsonPath = new JsonPath(String.valueOf(object));
-        return jsonPath.getJsonObject(keyPath).toString();
-    }
-
-    private static Map<?, ?> getDataAsMap(JSONObject object, String keyPath) {
-        jsonPath = new JsonPath(String.valueOf(object));
-        return jsonPath.getMap(keyPath);
-    }
-
-    private static List<?> getDataAsList(JSONObject object, String keyPath) {
-        jsonPath = new JsonPath(String.valueOf(object));
-        return jsonPath.getList(keyPath);
-    }
-
-    public enum Types {
+    public enum
+    Types {
         STRING, MAP, LIST
     }
 }
