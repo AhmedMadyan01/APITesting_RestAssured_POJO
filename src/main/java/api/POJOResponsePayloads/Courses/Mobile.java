a5ce180d9e3983bb078a335e5defad067c5aaa3c
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-03 03:36:43 +0200

    Update Mobile.java

--- src/main/java/api/POJOResponsePayloads/Courses/Mobile.java
+++ src/main/java/api/POJOResponsePayloads/Courses/Mobile.java
@@ -8,7 +8,15 @@ public class Mobile {
         return courseTitle;
     }
 
+    public void setCourseTitle(String courseTitle) {
+        this.courseTitle = courseTitle;
+    }
+
     public String getPrice() {
         return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
     }
 }
