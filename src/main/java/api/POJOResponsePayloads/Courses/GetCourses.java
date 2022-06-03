20be18f27fd64f3036bd2f67f961467c5716521d
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-03 03:23:52 +0200

    Update GetCourses.java

--- src/main/java/api/POJOResponsePayloads/Courses/GetCourses.java
+++ src/main/java/api/POJOResponsePayloads/Courses/GetCourses.java
@@ -1,7 +1,5 @@
 package api.POJOResponsePayloads.Courses;
 
-import java.util.ArrayList;
-
 public class GetCourses {
     private String url;
     private String services;
@@ -22,9 +20,6 @@ public class GetCourses {
         return expertises;
     }
 
-    //    public ArrayList<GetCourses.courses> getCourses() {
-//        return courses;
-//    }
     public Courses getCourses() {
         return courses;
     }
@@ -36,25 +31,4 @@ public class GetCourses {
     public String getLinkedIn() {
         return linkedIn;
     }
-
-//    private static class courses {
-//        ArrayList<webAutomation> webAutomations;
-//        ArrayList<api> apis;
-//        ArrayList<mobile> mobiles;
-//    }
-//
-//    private static class webAutomation {
-//        private String courseTitle;
-//        private String price;
-//    }
-//
-//    private static class api {
-//        private String courseTitle;
-//        private String price;
-//    }
-//
-//    private static class mobile {
-//        private String courseTitle;
-//        private String price;
-//    }
 }
