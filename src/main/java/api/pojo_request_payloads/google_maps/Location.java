bc0beba9e155622666589443443c7261a499e12b
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-14 01:38:42 +0200

    Create Location.java

---
+++ src/main/java/api/pojo_request_payloads/google_maps/Location.java
@@ -0,0 +1,22 @@
+package api.pojo_request_payloads.google_maps;
+
+public class Location {
+    private double lat;
+    private double ing;
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getIng() {
+        return ing;
+    }
+
+    public void setIng(double ing) {
+        this.ing = ing;
+    }
+}
