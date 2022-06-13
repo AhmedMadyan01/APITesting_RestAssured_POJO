2e786cc035c8b00579cb5cfe2f46269a8c29cfd3
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-14 01:45:13 +0200

    Update Type.java

--- src/main/java/api/pojo_request_payloads/google_maps/Type.java
+++ src/main/java/api/pojo_request_payloads/google_maps/Type.java
@@ -3,6 +3,8 @@ package api.pojo_request_payloads.google_maps;
 import java.util.List;
 
 public class Type {
+    private String type;
+
     public String getType() {
         return type;
     }
@@ -10,6 +12,4 @@ public class Type {
     public void setType(String type) {
         this.type = type;
     }
-
-    private String type;
 }
