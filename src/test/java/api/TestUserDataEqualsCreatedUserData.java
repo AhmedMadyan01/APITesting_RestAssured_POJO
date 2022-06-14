33abd6f2955878e7d60c1342be0f285856673b33
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-14 02:39:32 +0200

    Update TestUserDataEqualsCreatedUserData.java

--- src/test/java/api/TestUserDataEqualsCreatedUserData.java
+++ src/test/java/api/TestUserDataEqualsCreatedUserData.java
@@ -1,12 +1,12 @@
 package api;
 
-import api.pojo_request_payloads.CreateUsers;
+import api.pojo_request_payloads.reqres.CreateUser;
 import org.testng.annotations.Test;
 
 public class TestUserDataEqualsCreatedUserData {
     @Test
     public void createUser() {
-        CreateUsers createUser = new CreateUsers();
+        CreateUser createUser = new CreateUser();
         createUser.setName("Ahmed");
         createUser.setJob("Software Test Automation Engineer");
 //        User user = RestAssured.given().when().post("https://reqres.in/api/users").body().as(CreateUsers.class).(User.class);
