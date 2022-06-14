48dd62b48036c60f64e396f4996e38d580c931c2
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-14 02:39:26 +0200

    Update User.java

--- src/main/java/api/pojoResponsePayloads/CreateUser/User.java
+++ src/main/java/api/pojoResponsePayloads/CreateUser/User.java
@@ -1,26 +1,8 @@
 package api.pojoResponsePayloads.CreateUser;
 
 public class User {
-    private String name;
-    private String job;
     private String id;
     private String createdAt;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getJob() {
-        return job;
-    }
-
-    public void setJob(String job) {
-        this.job = job;
-    }
 
     public String getId() {
         return id;
