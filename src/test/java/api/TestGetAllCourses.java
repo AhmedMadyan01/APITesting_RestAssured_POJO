cfd77effebbc24c0874601bf4013caa8328c7193
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-16 02:34:25 +0200

    Update TestGetAllCourses.java

--- src/test/java/api/TestGetAllCourses.java
+++ src/test/java/api/TestGetAllCourses.java
@@ -1,9 +1,7 @@
 package api;
 
 import api.pojo_response_payload.all_courses.AllCourses;
-import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.parsing.Parser;
 import org.testng.annotations.Test;
 import utilities.api_driver.RequestBuilder;
 import utilities.api_driver.RequestMethod;
