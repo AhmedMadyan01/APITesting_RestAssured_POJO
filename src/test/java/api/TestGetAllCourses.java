8d540e6bd10d074d51bb2c154f9ab773b48e7ea1
Ahmed Madyan <ahmed.madyan@vodafone.com>
2022-06-18 21:47:20 +0200

    Update TestGetAllCourses.java

--- src/test/java/api/TestGetAllCourses.java
+++ src/test/java/api/TestGetAllCourses.java
@@ -2,6 +2,7 @@ package api;
 
 import api.pojo_response_payload.all_courses.AllCourses;
 import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
 import utilities.api_driver.RequestBuilder;
 import utilities.api_driver.RequestMethod;
@@ -13,7 +14,7 @@ public class TestGetAllCourses {
 //        AllCourses allCourses =
 //                RestAssured.given().expect().defaultParser(Parser.JSON).when().get("https://www.rahulshettyacademy.com/api/course").as(AllCourses.class);
         AllCourses allCourses =
-                RequestBuilder.performRequest("https://www.rahulshettyacademy.com", null, "/api/course", RequestMethod.GET, null, null, null, ContentType.JSON).as(AllCourses.class);
+                RequestBuilder.performRequest("https://www.rahulshettyacademy.com", null, "/api/course", null, RequestMethod.GET, null, null, null, ContentType.JSON, HttpStatus.SC_OK).as(AllCourses.class);
         System.out.println(allCourses.getCourses());
     }
 }
