1d53312412ad3931d80faf416df8664a9122ce6c
Ahmed Madyan <ahmed.madyan@vodafone.com>
2022-06-18 23:25:40 +0200

    Update ConfigUtils.java

--- src/main/java/utilities/reader_manager/properties_reader/ConfigUtils.java
+++ src/main/java/utilities/reader_manager/properties_reader/ConfigUtils.java
@@ -1,13 +1,13 @@
 package utilities.reader_manager.properties_reader;
 
 public class ConfigUtils {
-    private static final String config_Properties = "/src/main/resources/config.properties";
+    private static final String CONFIG_PROPERTIES_FILE_PATH = "/src/main/resources/config.properties";
     private static String requires_BaseURI;
     private static String requires_BasePath;
 
     private static void setConfigProperties() {
-        requires_BaseURI = PropertiesReaderManager.getProperties("requires_BaseURI", config_Properties);
-        requires_BasePath = PropertiesReaderManager.getProperties("requires_BasePath", config_Properties);
+        requires_BaseURI = PropertiesReaderManager.getProperties("requires_BaseURI", CONFIG_PROPERTIES_FILE_PATH);
+        requires_BasePath = PropertiesReaderManager.getProperties("requires_BasePath", CONFIG_PROPERTIES_FILE_PATH);
     }
 
     public static String getRequires_BaseURI() {
